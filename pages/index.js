101a502b3c33e692d88a42a0a2e6a8711637730d
Felipe O de Andrade <41307625+nickpobre@users.noreply.github.com>
2025-10-20 03:29:22 +0000

    Adicionando a formatação com Prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Avançar exige relembrar onde tudo começou.</h1>
+  return <h1>Avançar exige relembrar onde tudo começou.</h1>;
 }
 
-export default Home
+export default Home;
